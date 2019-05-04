1cbc5c054672110dfe99f5e8378e54713da002b8
csk <shivaranjani229@gmail.com>
2019-05-04 23:30:26 +0530

    push to gitbash

---
+++ src/test/java/sample5555/stylishtest.java
@@ -0,0 +1,14 @@
+package sample5555;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class stylishtest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
